6b2d95928f6f11061f11ca21cbb0db4cec3d052a
Pavlo <pasha.ny88@gmail.com>
2020-09-12 10:02:01 -0400

    1

---
+++ src/Class1/TestFile.java
@@ -0,0 +1,16 @@
+package Class1;
+
+public class TestFile {
+
+    public static void main(String[] args) {    //psvm
+
+        System.out.println("Hello World!");     //sout
+
+        byte age = 25;
+        int  data1 = 155;
+        long data2 = 77777777777L;
+        System.out.println(data1);
+        System.out.println(data2);
+        System.out.println(age);
+    }
+}
