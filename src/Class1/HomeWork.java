8bdeba0d4ac52d49ac742f6e5b0f64260d411af9
Pavlo <pasha.ny88@gmail.com>
2020-09-16 19:43:41 -0400

    HomeWork

--- src/Class1/HomeWork.java
+++ src/Class1/HomeWork.java
@@ -1,2 +1,26 @@
-package Class1;public class HomeWork {
+package Class1;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        byte hours = 24;
+        int year = 366;
+        short emp = 333;
+        long populationUsa = 328200000L;
+        float mortgageApr = 2.17f;
+        double accBalance = 357754.45;
+        boolean sunRise = false;
+        char firstName = 'P';
+        char lastName = 'A';
+        String myName = "Pavlo Andrukhov";
+        System.out.println ("Number of hours in a day is " + hours);
+        System.out.println ("Max nomber of days in an year is " + year);
+        System.out.println ("Total number of employees in an organization is " + emp);
+        System.out.println ("current population in the USA is " + populationUsa);
+        System.out.println ("Current mortgage interest rate is " + mortgageApr);
+        System.out.println ("Current balance in bank account is " + accBalance);
+        System.out.println ("Does sun rise from the west? " + sunRise);
+        System.out.println ("Initials of my name is " + firstName+lastName);
+        System.out.println ("My name is " + myName);
+
+    }
 }
